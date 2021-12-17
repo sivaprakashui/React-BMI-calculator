d05bca5733061fbbdc28ca87fbb05be3d096932f
unknown <sivaprakashui@gmail.com>
2021-12-18 02:19:52 +0530

    Updates

---
+++ precache-manifest.e7f7dcba10841b31781bc1f2c7b88cba.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3cb73b635c78213ebde1b64638367ddf",
+    "url": "/React-BMI-calculator/index.html"
+  },
+  {
+    "revision": "68ef8ee85d685902c090",
+    "url": "/React-BMI-calculator/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "687c6b07f6e2e57d3986",
+    "url": "/React-BMI-calculator/static/css/main.e0a46aff.chunk.css"
+  },
+  {
+    "revision": "68ef8ee85d685902c090",
+    "url": "/React-BMI-calculator/static/js/2.4e60f0c9.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/React-BMI-calculator/static/js/2.4e60f0c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "687c6b07f6e2e57d3986",
+    "url": "/React-BMI-calculator/static/js/main.c36a621f.chunk.js"
+  },
+  {
+    "revision": "31d4095b67928221836b",
+    "url": "/React-BMI-calculator/static/js/runtime-main.8c494a95.js"
+  }
+]);
