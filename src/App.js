591c0e68f3b9b83768ae531dde8dfc7a9e8a076a
unknown <sivaprakashui@gmail.com>
2021-12-18 02:12:09 +0530

    hello

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import './App.css';
+import BMI from './components/BMI';
+
+const App = () => {
+  return (
+    <div className="App container">
+      <div className="row justify-content-center">
+        <div className="col-lg-6 col-lg-offset-3">
+          <BMI />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
